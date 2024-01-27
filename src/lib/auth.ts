d4849908cc84468c3bbc51cb8179f8f11ef372ed
Kev <sulpeum@gmail.com>
2024-01-26 23:07:25 -0500

    fix: test module

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,17 @@
 import NextAuth, { NextAuthConfig, Session, User } from "next-auth";
-import GitHub from "next-auth/providers/GitHub";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@/db";
+import GitHubProvider from "next-auth/providers/github";
 
 export const authConfig = {
-  providers: [GitHub],
   adapter: DrizzleAdapter(db),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+      allowDangerousEmailAccountLinking: true,
+    }),
+  ],
   callbacks: {
     async session({ session, user }: { session: Session; user?: User }) {
       if (user && user.id) {
