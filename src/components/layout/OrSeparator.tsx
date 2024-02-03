bc95ccd1a1d347f677ebf7779ac4562d1fb5e0c7
Kev <sulpeum@gmail.com>
2024-02-03 17:00:18 -0500

    style: add font bricolage

--- src/components/layout/OrSeparator.tsx
+++ src/components/layout/OrSeparator.tsx
@@ -2,7 +2,7 @@ export const OrSeparator = () => {
   return (
     <div className="my-4 flex items-center gap-4">
       <div className="h-[1px] w-full bg-border"></div>
-      <div className="min-w-fit text-xs font-semibold uppercase tracking-tight text-gray">
+      <div className="min-w-fit font-bricolage text-xs font-semibold uppercase tracking-tight text-gray">
         or continue with
       </div>
       <div className="h-[1px] w-full bg-border"></div>
